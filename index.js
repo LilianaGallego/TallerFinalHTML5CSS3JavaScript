7682d1516cc56ed8d5259f51028524c19f3ce159
LilianaGallego <lilianagallegom@gmail.com>
2022-05-21 19:37:39 -0500

    mejorando el estilo de la pagina

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
-let listaImpar = document.getElementsByClassName('impar');
-let listaPar = document.getElementsByClassName('par');
-let fondo = document.querySelector('body')
-let resaltar = document.getElementsByClassName('resaltar')
+let listaImpar = document.getElementsByClassName("impar");
+let listaPar = document.getElementsByClassName("par");
+let fondo = document.querySelector("body");
+let resaltar = document.getElementsByClassName("resaltar");
 
-function impar(){
-    for (let i = 0; i < resaltar.length; i++) {
-        resaltar[i].style.background= 'lightpink';
-        listaImpar[i].style.color = 'red';
-        resaltar[i].style.color = 'red';
-        listaPar[i].style.background = 'none';
-        listaPar[i].style.color = 'rgb(212, 23, 149)';
-        listaPar[i].style.border = 'none';
-        
-    }   
-    fondo.style.backgroundColor= 'lightgreen'
+function impar() {
+  for (let i = 0; i < resaltar.length; i++) {
+    listaImpar[i].style.background = "lightpink";
+    listaImpar[i].style.color = "red";
+    listaImpar[i].style.border = "2px solid red";
+    listaPar[i].style.color = "rgb(65, 6, 55)";
+    listaPar[i].style.background = "none";
+    listaPar[i].style.border = "none";
+  }
+  
 }
 
-function par(){
-    
-    for (let i = 0; i < listaPar.length; i++) {
-        listaPar[i].style.background = 'lightpink';
-        listaPar[i].style.color = 'darkviolet';
-        listaPar[i].style.border = '1px solid red';
-        resaltar[i].style.background= 'none';
-        listaImpar[i].style.color = 'rgb(212, 23, 149)';
-        resaltar[i].style.color = 'rgb(212, 23, 149)';
-    }   
-    fondo.style.backgroundColor= 'lightsalmon'
+function par() {
+  for (let i = 0; i < listaPar.length; i++) {
+    listaPar[i].style.background = "lightpink";
+    listaPar[i].style.color = "red";
+    listaPar[i].style.border = "2px solid red";
+    listaImpar[i].style.color = "rgb(65, 6, 55)";
+    listaImpar[i].style.background = "none";
+    listaImpar[i].style.border = "none";
+  }
+  
 }
-
-
